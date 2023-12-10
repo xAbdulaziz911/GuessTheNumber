8138885ac040e63a1ccc35f3e26a4b13ecad3f08
MNJAA <hlmm8645@gmail.com>
2023-12-10 14:32:37 +0300

    less better

--- GuessTheNumber/GuessTheNumber0.1.py
+++ GuessTheNumber/GuessTheNumber0.1.py
@@ -1,30 +1,31 @@
 from random import randint
-s = 0
 modes = {"1": 10, "2": 50, "3": 200, "4": 10000, "5": 1000000000}
-
-for i, mode in enumerate(["Easy","Normal","Hard","Very Hard","Impossible"], start=1):
-    print(f"{i}- {mode}")
-N = input("Choose The Difficulty (enter a number): ")
-
-if N in modes:
-  print(f"Guess from 1 to {modes[N]}")
-  
-  N = modes[N]
-  x = randint(1, N)
-  
-else:
-  print("Invalid choice. Please choose a valid number.")
-
+user = 0
+x = 0
+s = 0
 while True:
-  user = int(input(">>: "))
-  s += 1
-  if user == x:
-    print("Correct!")
-    print("Number of tries: ",s)
+  for i, mode in enumerate(["Easy","Normal","Hard","Very Hard","Impossible"], start=1):
+    print(f"{i}- {mode}") 
+  N = input("Choose The Difficulty (enter a number): ")
+  if N in modes:
+    print(f"Guess from 1 to {modes[N]}")
+    N = modes[N]
+    x = randint(1, N)
     break
   else:
-    print("Missed it")
+    print("Invalid choice. Please choose a valid number.")
+while True:
+  try:
+    user = int(input(">>: "))
+    s += 1
+    if user == x:
+      print("Correct!")
+      print(f"Number of tries: {s}")
+      break
+    else:
+      print("Missed it")
+  except:ValueError
   if user > x:
-     print("lower!")
+    print("lower!")
   else:
     print("higher!")
